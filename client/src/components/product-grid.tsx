81bc2d4a6fb0356dda13bb8034611fa65fbe5a54
Dusk1234567 <your91776@gmail.com>
2025-08-04 15:14:24 +0530

    Update product-grid.tsx

--- client/src/components/product-grid.tsx
+++ client/src/components/product-grid.tsx
@@ -32,7 +32,7 @@ export default function ProductGrid() {
 
   const filterButtons = [
     { id: "all", label: "All Products", icon: null },
-    { id: "ranks", label: "Server", icon: Minecraft },
+    { id: "ranks", label: "Server", icon: ranks },
     { id: "coins", label: "VPS", icon: vps },
   ];
 
